d011c4ea8a28320175df23d33ec5004669cd971b
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2024-08-01 21:57:29 -0400

    Update helpers.lua

--- clientExports/helpers.lua
+++ clientExports/helpers.lua
@@ -273,8 +273,6 @@ local function ControlPlacement(previewModel, callback)
                 FreezeEntityPosition(previewModel, true)
                 if callback then
                     callback(true, previewModel, currentPos, currentRot)
-                else
-                    LogMessage('Callback for the PlaceModel export is nil', true, false, LogLevel.WARN)
                 end
                 break
             end
@@ -283,8 +281,6 @@ local function ControlPlacement(previewModel, callback)
                 DeleteEntity(previewModel)
                 if callback then
                     callback(false, nil)
-                else
-                    LogMessage('Callback for the PlaceModel export is nil', true, false, LogLevel.WARN)
                 end
                 break
             end
